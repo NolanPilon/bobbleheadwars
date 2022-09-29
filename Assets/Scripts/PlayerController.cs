a5baffbbb7b02a0aace419aaea6de66c6aaa3221
Nolan <npilon2003@gmail.com>
2022-09-29 17:42:28 -0400

    Chapter 4 - Added Player Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 50.0f;
     private CharacterController playerController;
     public Rigidbody head;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
     void Start()
     {
         playerController = GetComponent<CharacterController>();
@@ -30,6 +32,20 @@ public class PlayerController : MonoBehaviour
         else 
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore)) 
+        {
+            if (hit.point != currentLookTarget) 
+            {
+                currentLookTarget = hit.point;
+            }
+            Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+            transform.rotation =  Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
         }
     }
 }
