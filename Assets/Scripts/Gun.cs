90bd3c702423669d8c5a4bb84424f18061f6d335
Nolan <npilon2003@gmail.com>
2022-10-07 10:09:41 -0400

    Chapter 7 - Added Gun Upgrade and Firing Sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,9 +6,13 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPos;
+    private AudioSource audioSource;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
@@ -28,10 +32,35 @@ public class Gun : MonoBehaviour
     }
     void fireBullet() 
     {
-        //Spawn Bullet at Barrel Position
+       Rigidbody bullet = createBullet();
+       bullet.velocity = transform.parent.forward * 100;
+        if (isUpgraded) 
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else 
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
+    private Rigidbody createBullet() 
+    {
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         bullet.transform.position = launchPos.position;
-        //Add Forward Force
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun() 
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
